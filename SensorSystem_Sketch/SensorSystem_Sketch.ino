787072fc3c872039912d44cd1b45c175118b4a3c
Scott Camarena <scamaren@asu.edu>
2013-03-20 15:37:37 -0700

    Updates

--- SensorSystem_Sketch/SensorSystem_Sketch.ino
+++ SensorSystem_Sketch/SensorSystem_Sketch.ino
@@ -1,4 +1,4 @@
-//#include <DHT22.h>
+#include <DHT22.h>
 //#include <SPI.h>
 #include <SensorSystem.h>
 //#include <SoftwareSerial.h>
@@ -7,20 +7,26 @@
 #define 
 
 */
-char command;
+String command;
 
 void setup()
 {
-  Serial.begin(28800);
+  randomSeed(analogRead(0));
+  Serial.begin(9600);
+  Serial.println("Starting...");
   //SerialBT.begin(9600);
 }
 
 void loop()
 {
   // Read in command
+  Serial.println("Listening for command...");
   command = readInCommand();
+  Serial.println("Received: " + command);
+  
 
   // Execute command
+  Serial.println("Executing: " + command);
   if( execCommand(command) != 0 )
     Serial.println("*Invalid Command" );
   
