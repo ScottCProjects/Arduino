45e778e55c83473b811571484c778b0cd72c6827
Scott <scamaren@asu.edu>
2013-04-05 16:27:32 -0700

    Reworking Pressure Sensor class

--- Libraries/MPL115A1/MPL115A1.h
+++ Libraries/MPL115A1/MPL115A1.h
@@ -3,7 +3,8 @@
 // MPL115A1 Barometric Pressure Sensor
 // - Arduino Class
 //
-// Based on MPL115A1 ATmega328 C code by Jim Lindblom at sparkfun.com
+// Based on sample SCP1000 code from arduino.cc
+// http://arduino.cc/en/Tutorial/BarometricPressureSensor
 // 
 //============================================================
 
@@ -13,49 +14,31 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-#define PRESH	0x00	// 80
-#define	PRESL	0x02	// 82
-#define	TEMPH	0x04	// 84
-#define	TEMPL	0x06	// 86
-
-#define A0MSB	0x08	// 88
-#define A0LSB	0x0A	// 8A
-#define B1MSB	0x0C	// 8C
-#define B1LSB	0x0E	// 8E
-#define	B2MSB	0x10	// 90
-#define B2LSB	0x12	// 92
-#define C12MSB	0x14	// 94
-#define	C12LSB	0x16	// 96
-#define	C11MSB	0x18	// 98
-#define C11LSB	0x1A	// 9A
-#define C22MSB	0x1C	// 9C
-#define C22LSB	0x1E	// 9E
-
-// AVR Port macros
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif 
-
-#define Byte uint8_t
+#define byte uint8_t
+//Sensor's memory register addresses:
+#define PRESSURE = 0x1F;      //3 most significant bits of pressure
+#define PRESSURE_LSB = 0x20;  //16 least significant bits of pressure
+#define TEMPERATURE = 0x21;   //16 bit temperature reading
+#define READ = 0b11111100;     // SCP1000's read command
+#define WRITE = 0b00000010;   // SCP1000's write command
 
 class MPL115A1
 {
 private:
-	Byte sdn, csn, sdo, sdi, sck;
+	byte sdn, csn, sdo, sdi, sck;
 
+	/*
 	void writeData(char data);
 	char readData();
 	char read(uint8_t address);
 	void write(uint8_t address, char data);
 	float calculatePressure();
+	*/
 
 public:
 	// No Default constructor, must define pins
-	MPL115A1( Byte sdnP, Byte csnP,
-			Byte sdoP, Byte sdiP, Byte sckP )
+	MPL115A1( byte sdnP, byte csnP,
+			byte sdoP, byte sdiP, byte sckP )
 	: sdn(sdnP), csn(csnP), sdo(sdoP), sdi(sdiP), sck(sckP)
 	{}
 
@@ -64,12 +47,16 @@ public:
 
 int MPL115A1_testMain()
 {
+	/*
+	SDN: pin 9
+	CSN: pin 8
+	SDI: pin 11
+	SDO: pin 12
+	SCK: pin 13
+	*/
 	Serial.begin(9600);
 	MPL115A1 bp( 9, 8, 12, 11, 13 );
 	
-	init_SPI();
-	sbi( PORTB, CS );
-	sbi( PORTB, SDN );
 	return 0;
 }
 
